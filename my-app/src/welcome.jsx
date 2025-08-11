bb465ce31d3252d7702d5c44b711430a37aa96ed
Lakshmi Prasanna <137285484+prasanna-srinivas@users.noreply.github.com>
2025-08-11 11:53:29 +0530

    Initial commit of react project

---
+++ my-app/src/welcome.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Student from './Student.js';
+
+const Welcome = () => {
+    let name ="Prasanna"
+  return (
+    <div>
+    <h1>My Name</h1>
+    <p>{name}</p>
+    </div>
+  );
+};
+
+export default Welcome;
